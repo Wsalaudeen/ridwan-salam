9f141768150c11594dafb83d9eba5a24df3fb245
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-03-25 14:22:58 +0100

    testing image slider

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
+import NavBar from "./components/Navbar/Navbar";
+import Mainpage from "./components/Mainpage/Mainpage";
 import "./App.css";
-import { useState } from "react";
-import { NavLink } from "react-router-dom";
-import Hamburger from "hamburger-react";
-import "./Navbar.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
+      <Mainpage />
     </div>
-  );
-}
-
-function NavBar() {
-  const [showNavbar, setShowNavbar] = useState(false);
-
-  const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar);
-  };
-  return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="logo">
-          <h1>Ridwan Salam</h1>
-        </div>
-
-        <div
-          className="menu-icon"
-          onClick={handleShowNavbar}
-          aria-label="Hamburger menu"
-          role="Toggle menu"
-        >
-          <Hamburger />
-        </div>
-        <div className={`navbar-elements  ${showNavbar ? "active" : ""}`}>
-          <ul>
-            <li>
-              <NavLink to="/home">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/assignments">Assignments</NavLink>
-            </li>
-
-            <li>
-              <NavLink to="/projects">Projects</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact </NavLink>
-            </li>
-            <li></li>
-            <li>
-              <NavLink to="/ prints">Prints</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Hamburger from "hamburger-react";
+import "./Navbar.css";
+
+export default function NavBar() {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+  return (
+    <header className="header">
+      <nav
+        className="navbar"
+        style={{
+          backgroundColor: "#f1efe2",
+        }}
+      >
+        <div className="container">
+          <div className="logo">
+            <h1>Ridwan Salam</h1>
+          </div>
+
+          <div
+            className="menu-icon"
+            onClick={handleShowNavbar}
+            aria-label="Hamburger menu"
+            role="Toggle menu"
+          >
+            <Hamburger />
+          </div>
+          <div className={`navbar-elements  ${showNavbar ? "active" : ""}`}>
+            <ul>
+              <li>
+                <NavLink to="/home">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/assignments">Assignments</NavLink>
+              </li>
+
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact </NavLink>
+              </li>
+              <li>
+                <NavLink to="/ prints">Prints</NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
